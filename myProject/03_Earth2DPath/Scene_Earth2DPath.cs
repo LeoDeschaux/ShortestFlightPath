810fbd374d903e58b0ca5f7d42604c1392464017
LeoDeschaux <leo.db40@gmail.com>
2021-11-07 12:10:23 +0100

    0.0.04

--- myProject/03_Earth2DPath/Scene_Earth2DPath.cs
+++ myProject/03_Earth2DPath/Scene_Earth2DPath.cs
@@ -9,6 +9,8 @@ using myEngine;
 using zzMathVisu.myProject;
 using zzMathVisu.myProject._03_Earth2DPath;
 
+using Num = System.Numerics;
+
 namespace zzMathVisu
 {
     public class Scene_Earth2DPath : MathScene
@@ -37,17 +39,19 @@ namespace zzMathVisu
 
             camControl.isActive = true;
 
+            camera.ZoomOut(0);
+
             //Spawn Cities
             //MVUtil.SpawnPinAtCoords("Paris", Coord.Paris);
             //MVUtil.SpawnPinAtCoords("Mexico", Coord.Mexico);
 
             Marker a = new Marker("MarkerA");
-            a.latitude = 70;
-            a.longitude = 20;
+            a.latitude = -70;
+            a.longitude = -30;
 
             Marker b = new Marker("MarkerB");
-            b.latitude = -70;
-            b.longitude = -30;
+            b.latitude = 70;
+            b.longitude = 30;
 
             t = new Trajectory(a, b);
         }
@@ -64,9 +68,37 @@ namespace zzMathVisu
             //propertiesMenu.DrawGUI();
             //pathMenu.DrawGUI();
 
-            ImGui.Begin("DEBUG");
+            ImGui.SetNextWindowSize(new Num.Vector2(300, Settings.SCREEN_HEIGHT - 300));
+            ImGui.SetNextWindowPos(new Num.Vector2(Settings.SCREEN_WIDTH - 300, 0));
 
-            ImGui.DragFloat("MarkerA.Latitude", ref t.a.latitude);
+            ImGui.GetStyle().WindowRounding = 0.0f;
+            ImGui.GetStyle().ChildRounding = 0.0f;
+            ImGui.GetStyle().FrameRounding = 0.0f;
+            ImGui.GetStyle().GrabRounding = 0.0f;
+            ImGui.GetStyle().PopupRounding = 0.0f;
+            ImGui.GetStyle().ScrollbarRounding = 0.0f;
+
+            ImGuiWindowFlags window_flags = 0;
+
+            window_flags |= ImGuiWindowFlags.NoResize;
+            window_flags |= ImGuiWindowFlags.NoCollapse;
+            window_flags |= ImGuiWindowFlags.NoMove;
+
+            ImGui.SetNextWindowBgAlpha(1);
+
+            ImGui.Begin("WINDOW", window_flags);
+
+            ImGui.PushID("MarkerA");
+            ImGui.Text("Marker A");
+            ImGui.DragFloat("Latitude", ref t.a.latitude);
+            ImGui.DragFloat("Longitude", ref t.a.longitude);
+            ImGui.PopID();
+
+            ImGui.Text("Marker B");
+            ImGui.DragFloat("Latitude", ref t.b.latitude);
+            ImGui.DragFloat("Longitude", ref t.b.longitude);
+
+            ImGui.End();
         }
 
         
--- myProject/03_Earth2DPath/Trajectory.cs
+++ myProject/03_Earth2DPath/Trajectory.cs
@@ -34,67 +34,105 @@ namespace zzMathVisu.myProject._03_Earth2DPath
 
             DrawTrajectory(new Coord(a.latitude, a.longitude), new Coord(b.latitude, b.longitude), Color.Red);
 
-            Vector2 marker1Pos = MVUtil.ConvertCoordToVector(-a.latitude, a.longitude);
-            Vector2 marker2Pos = MVUtil.ConvertCoordToVector(-b.latitude, b.longitude);
-
-            Trajectory.DrawCrossHaitAtPoint(marker1Pos, matrix);
-            Trajectory.DrawCrossHaitAtPoint(marker2Pos, matrix);
-
             shapes.End();
         }
 
         private void DrawTrajectory(Coord a, Coord b, Color color)
         {
-
             Vector2 posA = MVUtil.ConvertCoordToVector(a.latitude, a.longitude);
             Vector2 posB = MVUtil.ConvertCoordToVector(b.latitude, b.longitude);
 
-            float distanceY = Math.Abs(posB.Y - posA.Y);
-            float distanceX = Math.Abs(posB.X - posA.X);
+            Trajectory.DrawCrossHaitAtPos(posA, Color.Red, SceneManager.currentScene.camera.transformMatrix);
+            Trajectory.DrawCrossHaitAtPos(posB, Color.LightGreen, SceneManager.currentScene.camera.transformMatrix);
 
-            t.s = "" + distanceY + ", posA: " + posA.Y + ", posB: " + posB.Y;
+            float distance = (posB - posA).Length();
+            t.s = "" + "distance : " + distance;
 
-            if(distanceY < 720/2 && distanceX < 1280/2)
-            {
-                shapes.DrawLine(posA * new Vector2(1, -1), posB * new Vector2(1, -1), 3, color);
-            }
-            else
-            {
-                if(posA.Y > posB.Y)
-                {
-                    shapes.DrawLine(posA * new Vector2(1, -1), new Vector2(0, -720/2), 3, color);
-                    shapes.DrawLine(posB * new Vector2(1, -1), new Vector2(0, 720 / 2), 3, color);
-                }
-                else
-                {
-                    shapes.DrawLine(posA * new Vector2(1, -1), new Vector2(0, 720/2), 3, color);
-                    shapes.DrawLine(posB * new Vector2(1, -1), new Vector2(0, -720/2), 3, color);
-                }
-                //shapes.DrawLine(posA * new Vector2(1, -1), posB * new Vector2(1, -1), 3, color);
-            }
+            DrawCursorEveryPos(posB);
+            DrawNet(posA, posB);
 
-            
+            //DRAW REVERSE NEAREST PATH
+            shapes.DrawLine(posB * new Vector2(1, -1),
+                (posB * new Vector2(1, -1)) - ((GetNearestPos(posA, posB) * new Vector2(1, -1)) - (posA * new Vector2(1, -1))) * new Vector2(1, 1),
+                3, color);
         }
 
-        private void DrawTrajectory(Coord a, Coord b, int segmentation, Color color)
+        private void DrawNet(Vector2 from, Vector2 to)
         {
-            Vector2 posA = MVUtil.ConvertCoordToVector(a.latitude, a.longitude);
-            Vector2 posB = MVUtil.ConvertCoordToVector(a.latitude, b.longitude);
+            foreach (Vector2 outsidePos in GetAllOutsidePos(to))
+            {
+                shapes.DrawLine(from * new Vector2(1, -1), outsidePos * new Vector2(1, -1), 3, Color.White);
+            }
 
-            Vector2 posC = MVUtil.ConvertCoordToVector(a.latitude, b.longitude);
-            Vector2 posD = MVUtil.ConvertCoordToVector(b.latitude, b.longitude);
+            shapes.DrawLine(from * new Vector2(1, -1), to * new Vector2(1, -1), 3, Color.White);
 
-            shapes.DrawLine(posA * new Vector2(1, -1), posB * new Vector2(1, -1), 3, color);
-            shapes.DrawLine(posC * new Vector2(1, -1), posD * new Vector2(1, -1), 3, color);
+            shapes.DrawLine(from * new Vector2(1, -1), GetNearestPos(from, to) * new Vector2(1, -1), 3, Color.Red);
         }
 
-        public static void DrawCrossHaitAtPoint(Vector2 pos, Matrix matrix)
+        private Vector2 GetNearestPos(Vector2 from, Vector2 to)
+        {
+            Vector2 result = to;
+
+            float distance = Vector2.Distance(from, to);
+
+            foreach(Vector2 outsidePos in GetAllOutsidePos(to))
+            {
+                float newDistance = Vector2.Distance(from, outsidePos);
+
+                if (newDistance < distance)
+                    result = outsidePos;
+            }
+
+            return result;
+        }
+
+        private List<Vector2> GetAllOutsidePos(Vector2 pos)
+        {
+            float width = 1280;
+            float height = 720;
+
+            Vector2 topLeft = new Vector2(pos.X - width, pos.Y + height);
+            Vector2 topMiddle = new Vector2(pos.X - 0, pos.Y + height);
+            Vector2 topRight = new Vector2(pos.X + width, pos.Y + height);
+
+            Vector2 middleLeft = new Vector2(pos.X - width, pos.Y + 0);
+            Vector2 middleRight = new Vector2(pos.X + width, pos.Y + 0);
+
+            Vector2 bottomLeft = new Vector2(pos.X - width, pos.Y - height);
+            Vector2 bottomMiddle = new Vector2(pos.X - 0, pos.Y - height);
+            Vector2 bottomRight = new Vector2(pos.X + width, pos.Y - height);
+
+            List<Vector2> result = new List<Vector2>();
+
+            result.Add(topLeft);
+            result.Add(topMiddle);
+            result.Add(topRight);
+
+            result.Add(middleLeft);
+            result.Add(middleRight);
+
+            result.Add(bottomLeft);
+            result.Add(bottomMiddle);
+            result.Add(bottomRight);
+
+            return result;
+        }
+
+        private void DrawCursorEveryPos(Vector2 pos)
+        {
+            foreach(Vector2 outsidePos in GetAllOutsidePos(pos))
+            {
+                Trajectory.DrawCrossHaitAtPos(outsidePos, Color.Black, SceneManager.currentScene.camera.transformMatrix);
+            }
+        }
+
+        public static void DrawCrossHaitAtPos(Vector2 pos, Color color, Matrix matrix)
         {
             Vector2 dimX = new Vector2(20, 4);
             Vector2 dimY = new Vector2(4, 20);
 
-            DrawSimpleShape.DrawRectangleFull(pos - dimX / 2, dimX, Color.Red, matrix);
-            DrawSimpleShape.DrawRectangleFull(pos - dimY / 2, dimY, Color.Red, matrix);
+            DrawSimpleShape.DrawRectangleFull((pos * new Vector2(1, -1)) - dimX / 2, dimX, color, matrix);
+            DrawSimpleShape.DrawRectangleFull((pos * new Vector2(1, -1)) - dimY / 2, dimY, color, matrix);
         }
     }
 }
